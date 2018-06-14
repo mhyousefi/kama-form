039a36f4110b56d25812ee7e5e2a6856aaad72e6
mhyousefi <mhyousefi96@gmail.com>
2018-06-14 15:33:51 +0430

    Folder restructuring

---
+++ React_Frontend/src/utils/QuestionInfo.js
@@ -0,0 +1,38 @@
+module.exports = {
+  'are you from UT': {
+    question: 'آیا دانشجو یا فارغ التحصیل معماری دانشگاه تهران هستید؟',
+    answers: ['بله', 'خیر']
+  },
+  'study level': {
+    question: 'مقطع تحصیلی:',
+    answers: ['کارشناسی', 'کارشناسی ارشد', 'دکتری',]
+  },
+  'areas of interest': {
+    question: 'در چه زمینه ای علاقه مند به تدریس هستید؟',
+    answers: [
+      'AutoCAD 2D',
+      'AutoCAD 3D',
+      'Photoshop PostPorduction',
+      'Photoshop شیت بندی',
+      'Illustrator',
+      'Coreldraw',
+      'Sketch Up',
+      'Revit',
+      'Rhinoceros',
+      'Grasshopper',
+      '3ds Max',
+      'Lumion',
+      'Twinmotion',
+      'اسکیس و راندو',
+      'عکاسی',
+    ]
+  },
+  'teaching experience': {
+    question: 'آیا سابقه تدریس دارید؟',
+    answers: ['بله', 'خیر']
+  },
+  'class timing': {
+    question: 'نحوه تشکیل کلاس:',
+    answers: ['هفتگی', 'فشرده-کارگاه'],
+  }
+}
